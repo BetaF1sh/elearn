6e09844f841e6db00052c6c609ec046db68cf3e1
Muhun Kim <iam@muhun.kim>
2018-09-01 11:30:29 +0900

    Use const and delete onload event

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-window.onload = function () {
-	const D = document
-	const $ = D.querySelector.bind(D)
-	const regex = RegExp('^http:\/\/common\.jejunu\.ac\.kr\/em\/[a-zA-Z0-9]*$');
-	const frame = $('iframe#bodyFrame')
+const D = document
+const $ = D.querySelector.bind(D)
+const regex = RegExp('^http:\/\/common\.jejunu\.ac\.kr\/em\/[a-zA-Z0-9]*$');
+const frame = $('iframe#bodyFrame')
 
-	var INTERVAL = 250;
-	var trigger = setInterval(function() {
-		if (regex.test(frame.src)){
-			open(frame.src, "_blank", "width=1060, height=689");
-			clearInterval(trigger)
-		}
-	}, INTERVAL);
-}
+const INTERVAL = 250;
+const trigger = setInterval(function() {
+	if (regex.test(frame.src)){
+		open(frame.src, "_blank", "width=1060, height=689");
+		clearInterval(trigger)
+	}
+}, INTERVAL);

0e7e5eeb2aed9dd75ecd1180aa12d4e9fd7e7b24
Muhun Kim <iam@muhun.kim>
2018-09-14 13:50:19 +0900

    Fix #1 and release v1.1
    use async

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-const D = document
-const $ = D.querySelector.bind(D)
 const regex = RegExp('^http:\/\/common\.jejunu\.ac\.kr\/em\/[a-zA-Z0-9]*$');
-const frame = $('iframe#bodyFrame')
+const frame = document.querySelector('iframe#bodyFrame')
 
-const INTERVAL = 250;
-const trigger = setInterval(function() {
+const INTERVAL = 100;
+const trigger = setInterval(async function() {
 	if (regex.test(frame.src)){
 		open(frame.src, "_blank", "width=1060, height=689");
 		clearInterval(trigger)
